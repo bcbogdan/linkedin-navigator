61abce016da65e5025f7e8e910d5c1ce3b310b02
Bogdan Carpusor <bogdan.carpusor@gmail.com>
2016-09-05 14:17:30 +0000

    Initial commit

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,12 +1,56 @@
 import express from 'express';
 import logger  from 'morgan';
+import google from 'google';
+import url from 'url';
 
 const router = express.Router();
-router.use(logger('dev'))
+let nextCounter = 0;
+google.resultsPerPage = 55;
+router.use(logger('dev'));
 
 router.get('/', (req, res) => {
-
   res.send({message: 'Hello World!!'});
+});
+
+const validateLink = (link) => {
+  const parsedUrl = url.parse(link);
+
+  if (parsedUrl.host.search("linkedin") == -1) {
+    return false;
+  } else if (parsedUrl.path.split('/')[1] != 'in') {
+    return false;
+  }
+
+  return true;
+};
+
+router.get('/:searchParams', (req, res) => {
+  const searchExpression = req.params.searchParams.split('-').join(' ');
+  let searchedLinks = [];
+  console.log(`Searching for ${searchExpression}`);
+
+  google(searchExpression, (err, searchResult) => {
+    if (err) {
+      console.error(err);
+    }
+
+    searchResult.links.forEach( item => {
+      if (validateLink(item.link)) {
+        searchedLinks.push(item)
+      }
+    });
+
+    if (nextCounter < 4) {
+      nextCounter += 1;
+      if (searchResult.next) {
+        searchResult.next();
+      } else {
+        res.json({
+          result: searchedLinks
+        });
+      }
+    }
+  });
 });
 
 export default router;
